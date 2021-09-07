f664616b47f467d651edc3fffd3b909cb8881bf2
histbaytbnk <90271476+histbaytbnk@users.noreply.github.com>
2021-09-07 18:15:03 +0200

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("")]
+[assembly: AssemblyTrademark("")]
+[assembly: Guid("e4d8486b-c87c-4e72-94e7-807f9088ebd3")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("PS4 Cheatos Inc")]
+[assembly: AssemblyCompany("Cheatos Inc")]
+[assembly: AssemblyProduct("PS4 Chetos Inc Save Special")]
+[assembly: AssemblyCopyright("© 2021 PS4 Cheatos Inc")]
+[assembly: AssemblyVersion("1.0.0.0")]
